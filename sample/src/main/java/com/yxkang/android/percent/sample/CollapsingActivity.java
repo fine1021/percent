c830e7ad526fd53a2f8c953d1d972943ddde2d1c
fine1021 <1078041387@qq.com>
2015-11-03 21:31:03 +0800

    add AppBarLayout extend anim

--- sample/src/main/java/com/yxkang/android/percent/sample/CollapsingActivity.java
+++ sample/src/main/java/com/yxkang/android/percent/sample/CollapsingActivity.java
@@ -1,12 +1,18 @@
 package com.yxkang.android.percent.sample;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MotionEvent;
 
 public class CollapsingActivity extends AppCompatActivity {
+
+    private static final String TAG = "CollapsingActivity";
+
+    private AppBarLayout appBarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,26 @@ public class CollapsingActivity extends AppCompatActivity {
         CollapsingToolbarLayout collapsingToolbar =
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle("Title");
+
+        appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_UP:
+                float range = appBarLayout.getTotalScrollRange();
+                float height = appBarLayout.getMeasuredHeight();
+                float offset = appBarLayout.getBottom() + range - height;
+                float rate = offset / range;
+                if (rate <= 0.8f) {
+                    appBarLayout.setExpanded(false, true);
+                }
+                break;
+            default:
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
     }
 
     @Override
